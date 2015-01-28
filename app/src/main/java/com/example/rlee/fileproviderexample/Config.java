98e4f8ef61b401e0fc150ac0cdc67eadd1490fd9
Richard Lee <lifuzu@gmail.com>
2015-01-27 18:52:15 -0800

    Support to upload image.

---
+++ app/src/main/java/com/example/rlee/fileproviderexample/Config.java
@@ -0,0 +1,12 @@
+package com.example.rlee.fileproviderexample;
+
+/**
+ * Created by rlee on 1/27/15.
+ */
+public class Config {
+    // File upload url (replace the ip with your server address)
+    public static final String FILE_UPLOAD_URL = "http://lab.weimed.com:3100/api/upload";
+
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+}
--- app/src/main/java/com/example/rlee/fileproviderexample/MainActivity.java
+++ app/src/main/java/com/example/rlee/fileproviderexample/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.rlee.fileproviderexample;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Message;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.ActionBarActivity;
@@ -13,11 +16,27 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
@@ -26,10 +45,24 @@ import java.net.URLConnection;
 
 public class MainActivity extends ActionBarActivity {
 
+    // LogCat tag
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // Upload function
+    private Button uploadButton;
+    private ProgressBar progressBar;
+    private TextView txtPercentage;
+    private ImageView imgPreview;
+    private VideoView vidPreview;
+    long totalSize = 0;
+
     // Progress Bar and update handler
     ProgressDialog progressDialog;
     Handler handler;
     private static final int DOWNLOAD_COMPLETED = 0;
+
+
+//    String uploadServerUri = "http://localhost:8888/?json_route=/media";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +80,38 @@ public class MainActivity extends ActionBarActivity {
                 }
             }
         };
+
+        // Upload function
+        txtPercentage = (TextView) findViewById(R.id.txtPercentage);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        imgPreview = (ImageView) findViewById(R.id.imgPreview);
+        vidPreview = (VideoView) findViewById(R.id.videoPreview);
+        uploadButton = (Button) findViewById(R.id.btnUpload);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // uploading the file to server
+                new UploadFileToServer().execute();
+            }
+
+//        uploadButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                progressDialog = progressDialog.show(MainActivity.this, "Upload", "Uploading file...", true);
+//
+//                new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        runOnUiThread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                uploadButton.setText("Uploading started ...");
+//                            }
+//                        });
+//                        uploadFile("abc.jpg", uploadServerUri);
+//                    }
+//                }).start();
+        });
     }
 
     @Override
@@ -166,5 +231,117 @@ public class MainActivity extends ActionBarActivity {
                 Intent.FLAG_GRANT_READ_URI_PERMISSION);
         Log.i("FILE_URI", "File Uri is " + Uri.fromFile(file));
         startActivity(intent);
+    }
+
+    /**
+     * Uploading the file to server
+     * */
+    private class UploadFileToServer extends AsyncTask<Void, Integer, String> {
+        @Override
+        protected void onPreExecute() {
+            // setting progress bar to zero
+            progressBar.setProgress(0);
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            // Making progress bar visible
+            progressBar.setVisibility(View.VISIBLE);
+
+            // updating progress bar value
+            progressBar.setProgress(progress[0]);
+
+            // updating percentage value
+            txtPercentage.setText(String.valueOf(progress[0]) + "%");
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            return uploadFile();
+        }
+
+        @SuppressWarnings("deprecation")
+        private String uploadFile() {
+            String responseString = null;
+
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(Config.FILE_UPLOAD_URL);
+
+            try {
+                AndroidMultiPartEntity entity = new AndroidMultiPartEntity(
+                        new AndroidMultiPartEntity.ProgressListener() {
+
+                            @Override
+                            public void transferred(long num) {
+                                publishProgress((int) ((num / (float) totalSize) * 100));
+                            }
+                        });
+
+//                MultipartEntity entity = new MultipartEntity();
+//                File sourceFile = new File(filePath);
+                File sourceFile = new File(getFilesDir(), "abc.jpg");
+
+//                entity.addPart("file", new FileBody(sourceFile, "image/jpeg"));
+                // Adding file data to http body
+                entity.addPart("image", new FileBody(sourceFile));
+
+                // Extra parameters if you want to pass to server
+//                entity.addPart("website",
+//                        new StringBody("www.weimed.com"));
+//                entity.addPart("email", new StringBody("abc@gmail.com"));
+
+                totalSize = entity.getContentLength();
+                httppost.setEntity(entity);
+
+                // Making server call
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity r_entity = response.getEntity();
+
+                int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode == 200) {
+                    // Server response
+                    responseString = EntityUtils.toString(r_entity);
+                } else {
+                    responseString = "Error occurred! Http Status Code: "
+                            + statusCode;
+                }
+
+            } catch (ClientProtocolException e) {
+                responseString = e.toString();
+            } catch (IOException e) {
+                responseString = e.toString();
+            }
+
+            return responseString;
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.e(TAG, "Response from server: " + result);
+
+            // showing the server response in an alert dialog
+            showAlert(result);
+
+            super.onPostExecute(result);
+        }
+
+    }
+
+    /**
+     * Method to show alert dialog
+     * */
+    private void showAlert(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message).setTitle("Response from Servers")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // do nothing
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
